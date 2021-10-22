d5c47f6c8445130800ddc2830f5cc8267b8532df
vleng-gangkangkung <90861876+vleng-gangkangkung@users.noreply.github.com>
2021-10-22 13:45:18 +0100

    Updated JS Weather Object

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("Hello GIT")
+alert("Hello GIT");
+
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true,
+};
+
+//challenge 1
+console.log(paris.temperature);
+
+//challenge 2
+let sydney = {
+  name: "Sydney",
+  country: "Australia",
+  language: "English",
+  temperature: 28,
+  capitalCity: false,
+};
+
+console.log(sydney.temperature);
